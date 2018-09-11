7122c08ffb21332a9f7ceb6748c0d5ec1bd56907
aschs <632266504@qq.com>
2018-09-12 01:20:04 +0800

    aop式事务注解

---
+++ agilebpm-base-spring-boot-samples/src/main/java/com/dstz/agilebpm/base/samples/config/TransactionAdviceConfig.java
@@ -0,0 +1,58 @@
+package com.dstz.agilebpm.base.samples.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+/**
+ * <pre>
+ * 描述：aop式事务注解
+ * 作者:aschs
+ * 邮箱:aschs@qq.com
+ * 日期:2018年9月12日
+ * 版权:summer
+ * </pre>
+ */
+@Configuration
+@Aspect
+public class TransactionAdviceConfig {
+	private static final String AOP_POINTCUT_EXPRESSION = "execution(* com.dstz.*.*.manager.*.*(..))||execution(* com.dstz.*.manager.*.*(..))";
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Bean
+	public TransactionInterceptor txAdvice() {
+
+		DefaultTransactionAttribute requiredDTA = new DefaultTransactionAttribute();
+		requiredDTA.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+		DefaultTransactionAttribute requiredReadonlyDTA = new DefaultTransactionAttribute();
+		requiredReadonlyDTA.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		requiredReadonlyDTA.setReadOnly(true);
+
+		NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+		source.addTransactionalMethod("*", requiredDTA);
+		source.addTransactionalMethod("get*", requiredReadonlyDTA);
+		source.addTransactionalMethod("query*", requiredReadonlyDTA);
+		source.addTransactionalMethod("find*", requiredReadonlyDTA);
+		source.addTransactionalMethod("is*", requiredReadonlyDTA);
+		return new TransactionInterceptor(transactionManager, source);
+	}
+
+	@Bean
+	public Advisor txAdviceAdvisor() {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
+		return new DefaultPointcutAdvisor(pointcut, txAdvice());
+	}
+	
+}
